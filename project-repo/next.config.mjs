af5550b023e212b339a9127f85dd7f76f1043ce7
owenwongny <efusionresearch@gmail.com>
2024-09-30 01:43:32 +0800

    update

--- project-repo/next.config.mjs
+++ project-repo/next.config.mjs
@@ -3,10 +3,12 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/my-app/project-repo' : '',
+    assetPrefix: isProd ? '/my-app/project-repo' : '',
     images: {
-        unoptimized: true,
+        unoptimized: true, // Disable Next.js image optimization
     },
+    distDir: 'docs', // Specify docs as the output directory
 };
 
 export default nextConfig;
-
