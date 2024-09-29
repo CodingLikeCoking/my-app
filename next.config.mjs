0b67bbd040bcf19f39627da7a6eebea674e94179
owenwongny <efusionresearch@gmail.com>
2024-09-30 02:08:16 +0800

    changed basepath to my-app

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: isProd ? '/my-app/project-repo' : '',
-    assetPrefix: isProd ? '/my-app/project-repo/' : '',
+    basePath: isProd ? '/my-app' : '',
+    assetPrefix: isProd ? '/my-app/' : '',
     images: {
         unoptimized: true, // Disable Next.js image optimization
     },
